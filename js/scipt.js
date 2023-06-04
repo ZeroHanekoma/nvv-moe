87da39b25ed745d384448f87d0837761880f4ac2
nvvv <67618133+ZeroHanekoma@users.noreply.github.com>
2023-06-03 17:00:32 -0700

    updated layout
    and either made it so there's a light mode/dark mode toggle or broke that shit completely. i don't really understand js.

--- js/scipt.js
+++ js/scipt.js
@@ -1,1 +1,27 @@
-//Nothing is here yet.
+// script.js
+
+function toggleDarkMode() {
+    const body = document.body;
+    const linkElement = document.getElementById('theme-link');
+  
+    body.classList.toggle('dark-mode');
+  
+    if (body.classList.contains('dark-mode')) {
+      linkElement.href = '/css/style-dark.css';
+      localStorage.setItem('darkMode', 'enabled');
+    } else {
+      linkElement.href = '/css/style.css';
+      localStorage.setItem('darkMode', 'disabled');
+    }
+  }
+  
+  document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
+  
+  // Check if dark mode preference is saved
+  const savedPreference = localStorage.getItem('darkMode');
+  
+  if (savedPreference === 'enabled') {
+    document.body.classList.add('dark-mode');
+    document.getElementById('theme-link').href = '/css/style-dark.css';
+  }
+  
