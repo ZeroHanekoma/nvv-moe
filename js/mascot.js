3e05f670c3046984d6dbc4bdb0103ce1968e9310
nvvv <67618133+ZeroHanekoma@users.noreply.github.com>
2024-04-05 06:33:01 -0700

    Add files via upload

--- js/mascot.js
+++ js/mascot.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
     var mascotImage = document.querySelector('.mascot');
-    var randomImageNumber = Math.floor(Math.random() * 28) + 1;
+    var randomImageNumber = Math.floor(Math.random() * 50) + 1;
     mascotImage.src = 'img/mascot/' + randomImageNumber + '.png';
 });
