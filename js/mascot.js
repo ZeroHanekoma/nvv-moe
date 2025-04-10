341c4a6a7abf55b9cb0bf021b0d5b4eff5f5bacc
nvvv <67618133+ZeroHanekoma@users.noreply.github.com>
2025-04-10 09:25:07 -0700

    Add files via upload
    updated the mascot cap so the new mascots actually display.

--- js/mascot.js
+++ js/mascot.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
     var mascotImage = document.querySelector('.mascot');
-    var randomImageNumber = Math.floor(Math.random() * 72) + 1;
+    var randomImageNumber = Math.floor(Math.random() * 84) + 1;
     mascotImage.src = 'img/mascot/' + randomImageNumber + '.png';
 });
